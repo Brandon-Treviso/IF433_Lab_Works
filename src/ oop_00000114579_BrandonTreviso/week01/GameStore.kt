75643e806312a4b0eff184cd649f9362425713f0
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-02-03 16:21:51 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/ oop_00000114579_BrandonTreviso/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package ` oop_00000114579_BrandonTreviso`.week01
+
+fun main() {
+    val gameTitle = "Black Myth Wukong"
+    val price = 750000
+
+    println("Saya ingin beli sebuah game $gameTitle dengan harga $price")
+}
