292c27b8881cd51a55f3b89512de7aa2e12bfa5b
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-02-10 16:30:28 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000114579_BrandonTreviso/week02/Hero.kt
@@ -0,0 +1,13 @@
+package oop_00000114579_BrandonTreviso.week02
+
+class Hero (
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) hp = 0
+    }
+    fun isAlive(): Boolean = hp > 0
+}
