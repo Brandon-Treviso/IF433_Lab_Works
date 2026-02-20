aa311860ca93f2f99f1d6abcac74425c7ebd8801
Brandon-Treviso30 <brandon.treviso@student.umn.ac.id>
2026-02-20 14:50:53 +0700

    week03: fix setter using backing field

--- src/oop_00000114579_BrandonTreviso/week03/Main.kt
+++ src/oop_00000114579_BrandonTreviso/week03/Main.kt
@@ -3,5 +3,5 @@ package oop_00000114579_BrandonTreviso.week03
 fun main() {
     val e = Employee("Budi")
     println("--- Test Error ---")
-    e.salary = 5000 // Ini akan meledakkan program Anda
+    e.salary = 5000
 }
