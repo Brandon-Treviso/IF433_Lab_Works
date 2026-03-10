d8b240ea4cb3157bfe20cc529d731ffba5d94de3
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-03-10 16:06:12 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000114579_BrandonTreviso/week06/Main.kt
+++ src/oop_00000114579_BrandonTreviso/week06/Main.kt
@@ -23,4 +23,15 @@ fun main () {
     val lamp = SmartLamp("1", "Ruang Tamu")
     val speaker = SmartSpeaker("2", "Google Nest Dapur")
     val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n=== TESTING SECURITY ===")
+    hub.activateSecurityMode()
+
+    println("\n=== TURN OFF ALL SWITCHES ===")
+    hub.turnOffAllSwitches()
 }
