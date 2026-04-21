a8c3016d89111f6e5598c3144befb96df66931cd
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-04-21 15:14:23 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000114579_BrandonTreviso/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000114579_BrandonTreviso.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        Tradelog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        Tradelog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        Tradelog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        Tradelog("ETHUSDT", "LONG", 20, 25.0, "CLOSED"),
+        Tradelog("XRPUSDT", "LONG", 5, 0.0, "OPEN"),
+        Tradelog("SOLUSDT", "SHORT", 10, -3.5, "CLOSED")
+    )
+}
