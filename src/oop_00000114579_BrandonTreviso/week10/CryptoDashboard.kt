7fddb443def396beacd4d9b874711f41ff065074
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-04-28 15:25:40 +0700

    week10: (task) populate coin repository

--- src/oop_00000114579_BrandonTreviso/week10/CryptoDashboard.kt
+++ src/oop_00000114579_BrandonTreviso/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000114579_BrandonTreviso.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
 }
