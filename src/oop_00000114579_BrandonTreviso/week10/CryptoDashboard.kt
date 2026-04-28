853c7ddadd7714df7bafed98920cfb0b16d54df8
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-04-28 15:26:39 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000114579_BrandonTreviso/week10/CryptoDashboard.kt
+++ src/oop_00000114579_BrandonTreviso/week10/CryptoDashboard.kt
@@ -3,6 +3,7 @@ package oop_00000114579_BrandonTreviso.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
 
+    val response = ApiResponse("200 OK", coinRepo.getAll())
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 1000.0))
