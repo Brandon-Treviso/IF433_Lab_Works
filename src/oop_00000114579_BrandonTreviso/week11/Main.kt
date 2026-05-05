2538581847dfbc6aaa4f319f1261be3ff5a599a6
Brandon-Treviso30 <brandon.treviso@gmail.com>
2026-05-05 15:05:58 +0700

    week11: implement apply for object initialization

--- src/oop_00000114579_BrandonTreviso/week11/Main.kt
+++ src/oop_00000114579_BrandonTreviso/week11/Main.kt
@@ -1,10 +1,17 @@
 package oop_00000114579_BrandonTreviso.week11
 
 fun main() {
-    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("\n=== TEST EXTENSION FUNCTIONS ===")
     println("Alex".addGreeting())
     println("Hi".repeatTimes(3))
 
     val text: String? = null
     println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+
+    println("\n=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
 }
